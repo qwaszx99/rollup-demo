3d59a0a70383e88520ee78073677e03dae759388
xingyongsheng <xingyongsheng@metamedical.cn>
2023-08-20 23:57:58 +0800

    添加忽略文件

--- dist/bundle.esm.js
+++
@@ -1,2 +0,0 @@
-import e from"react";function m(){return e.createElement("div",{className:"page"},"mmmm")}export{m as default};
-//# sourceMappingURL=bundle.esm.js.map
--- dist/bundle.js
+++
@@ -1,2 +0,0 @@
-"use strict";var e=require("react");module.exports=function(){return e.createElement("div",{className:"page"},"mmmm")};
-//# sourceMappingURL=bundle.js.map
